2182c87b271f1da09a399d291f43692fdda401a1
smoming <smoming1031@gmail.com>
2017-04-11 23:38:15 +0800

    WebView_Example
    WebView Example

---
+++ WebView_Example/HtmlWebView_Example.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace WebView_Example
+{
+	public partial class HtmlWebView_Example : ContentPage
+	{
+		public HtmlWebView_Example()
+		{
+			InitializeComponent();
+
+			string xHtml = "<Div><H1>Xamarin</H1><H2>Xamarin</H2><H3>Xamarin</H3></Div>";
+			var source = new HtmlWebViewSource();
+			source.Html = xHtml;
+			xWebView.Source = source;
+		}
+	}
+}
---
+++ WebView_Example/WebView_ExamplePage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace WebView_Example
+{
+	public partial class WebView_ExamplePage : ContentPage
+	{
+		public WebView_ExamplePage()
+		{
+			InitializeComponent();
+		}
+
+		void Handle_Navigating(object sender, Xamarin.Forms.WebNavigatingEventArgs e)
+		{
+			//throw new NotImplementedException();
+			xActivityIndicator.IsRunning = true;
+		}
+
+		void Handle_Navigated(object sender, Xamarin.Forms.WebNavigatedEventArgs e)
+		{
+			//throw new NotImplementedException();
+			xActivityIndicator.IsRunning = false;
+		}
+	}
+}
